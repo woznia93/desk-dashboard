8fd6ae52eb0d5c5a0e60e254bc49bec9d569900b
THEGOAT <woznia93@msu.edu>
2026-01-09 01:38:46 -0500

    up and running full UI + backend

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,19 @@
-function updateWeather() {
-    fetch("http://localhost:8000/weather");
-        .then(res=> res.json)
-        .then(data => {
-            document.getElementById("temp").innerText = data.temp + "°";
-            document.getElementById("condition").innerText = data.condition;
-            document.getElementById("clock").innerText = data.time;
-        })
-        .catch(err => {
-           console.error("Fetch error:", err));
-           document.getElementById("temp").inner.Text = "--o";
+async function updateWeather() {
+    try {
+    	const res = await fetch("http://127.0.0.1:8000/weather");
+    	const data = await res.json();
+    	
+        document.getElementById("temp").innerText = data.temp + "°";
+        document.getElementById("condition").innerText = data.condition;
+        document.getElementById("clock").innerText = data.time;
+        }
+    catch (err) {
+           console.error("Fetch error:", err);
+           document.getElementById("temp").inner.Text = "--°";
            document.getElementById("condition").innerText = "N/A";
 	   document.getElementById("clock").innerText = "--:--";
-        });
+        }
 }
+
 setInterval(updateWeather, 60000);
 updateWeather();
