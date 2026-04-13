3a206e3af45057770dcb45a3b8eb38a3427724f9
Brady Wozniak <143364354+woznia93@users.noreply.github.com>
2026-04-13 03:05:24 -0400

    Enhance chromium-browser startup options
    Added additional flags to chromium-browser command for improved startup behavior.

--- start-chromium.sh
+++ start-chromium.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# wait for 10 seconds to make sure server is ready
-
 sleep 10
-chromium-browser http://localhost:3000 --kiosk --incognito --noerrdialogs --disable-infobars
 
+chromium-browser http://localhost:3000 \
+  --kiosk \
+  --incognito \
+  --noerrdialogs \
+  --disable-infobars \
+  --disable-session-crashed-bubble \
+  --disable-restore-session-state \
+  --start-fullscreen \
+  --window-position=0,0 \
+  --window-size=1920,1080 \
+  --disable-translate \
+  --no-first-run \
+  --fast \
+  --fast-start
